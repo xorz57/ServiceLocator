c1e2baf1a0842074f6cf9ab6aa1116c05fce3f76
George Fotopoulos <xorz57@gmail.com>
2023-05-09 23:07:39 +0300

    Update ServiceLocator.hpp

--- src/ServiceLocator.hpp
+++ src/ServiceLocator.hpp
@@ -12,20 +12,22 @@ public:
 
     template<typename T, typename... Args>
     void SetInstance(Args... args) {
-        std::shared_ptr<T> instance = std::make_shared<T>(args...);
         const size_t hash = typeid(T).hash_code();
+        std::shared_ptr<T> instance = std::make_shared<T>(args...);
         mInstances.try_emplace(hash, std::shared_ptr<void>(instance));
     }
 
     template<typename T>
     std::shared_ptr<T> GetInstance() {
-        if (const size_t hash = typeid(T).hash_code(); mInstances.contains(hash)) {
+        const size_t hash = typeid(T).hash_code();
+        auto it = mInstances.find(hash);
+        if (it != mInstances.end()) {
             return std::static_pointer_cast<T>(mInstances.at(hash));
         }
         return nullptr;
     }
 
-    [[maybe_unused]] void Clear() {
+    void Clear() {
         mInstances.clear();
     }
 
